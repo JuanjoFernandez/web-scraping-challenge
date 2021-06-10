1b151f6c95502ae5ebf64493fd3932023b1657ad
Juanjo <juanjo_fdez@yahoo.com.mx>
2021-06-10 12:16:25 -0500

    Drops Mongo colection if exists

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 # Initialize PyMongo to work with MongoDBs
 conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
-db = client.mars_db
+db = client.planets
 
 app = Flask(__name__)
 
@@ -19,9 +19,12 @@ def home():
 @app.route("/scrape")
 def scrape():
     from scrape_mars import scrape
+    drop_collection = "mars" in db.list_collection_names()
+    if drop_collection == True:
+        db.mars.drop()
     data = scrape()
-    db.insert_one(data)
-    return
+    db.mars.insert_one(data)
+    return "done"
 
 if __name__ == "__main__":
     app.run(debug=True)
